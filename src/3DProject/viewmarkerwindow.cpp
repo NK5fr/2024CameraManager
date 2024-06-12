5875eb555e7de675888a415e2b0d09307d0288f8
Armand Sady <armand.sady.etu@univ-lille.fr>
2024-06-12 09:26:05 +0200

    added simple documentation to viewMarkerWindow

--- src/3DProject/viewmarkerwindow.cpp
+++ src/3DProject/viewmarkerwindow.cpp
@@ -10,12 +10,21 @@ ViewMarkerWindow::ViewMarkerWindow(QWidget *parent)
 {
     markerContainer = new QVBoxLayout(this);
 }
-
+/**
+ * @brief ViewMarkerWindow::setData
+ * Simply sets the data attribute to whatever you put as an argument and then tried to populate the layour with the default data.
+ * @param pointerToData pointer to the data you wish to load.
+ */
 void ViewMarkerWindow::setData(const Data *pointerToData) {
     data = pointerToData;
     populateContainer(0);
 }
 
+/**
+ * @brief ViewMarkerWindow::populateContainer
+ * This function makes a Layout filled with the data we need to display for every marker.
+ * @param step the current frame the data is on
+ */
 void ViewMarkerWindow::populateContainer(int step) {
     clearContainer();
     QVector<Marker> listMarkers = data->get1Vector(step);
@@ -24,7 +33,10 @@ void ViewMarkerWindow::populateContainer(int step) {
         markerContainer->addLayout(markerLayout);
     }
 }
-
+/**
+ * @brief ViewMarkerWindow::clearContainer
+ * This function empties markerContainer.
+ */
 void ViewMarkerWindow::clearContainer() {
     //QList<QHBoxLayout*> markerLayouts = this->layout()->findChildren<QHBoxLayout*>();
     QHBoxLayout *currentItem;
@@ -38,7 +50,11 @@ void ViewMarkerWindow::clearContainer() {
         delete currentItem;
     }
 }
-
+/**
+ * @brief ViewMarkerWindow::selectMarker
+ * This function sends a signal so that coordinateWindow so that it knows which marker has been selected.
+ * It also locks the button that lets you select a marker
+ */
 void ViewMarkerWindow::selectMarker() {
     QPushButton *senderButton = static_cast<QPushButton*>(sender());
     senderButton->setEnabled(false);
@@ -46,6 +62,11 @@ void ViewMarkerWindow::selectMarker() {
     emit markerPicked(index);
 }
 
+/**
+ * @brief ViewMarkerWindow::removedPickedMarker
+ * This function is a SLOT that gets called when you click the "remove" button in coordinateWindow, it lets you be able to click the "select" button again.
+ * @param index the index of the marker that got "un"-selected.
+ */
 void ViewMarkerWindow::removedPickedMarker(int index)
 {
     QHBoxLayout *goodLayout = static_cast<QHBoxLayout*>(markerContainer->itemAt(index));
@@ -57,6 +78,13 @@ void ViewMarkerWindow::removedPickedMarker(int index)
     }
 }
 
+/**
+ * @brief ViewMarkerWindow::addMarker
+ * This function creates a Layout containing the name, coordinates and buttons associated with a marker.
+ * @param marker the instance of the selected marker, it contains the necessary data.
+ * @param index the index of the marker
+ * @return an instance of QHBoxLayout that's then added to markerContainer.
+ */
 QHBoxLayout* ViewMarkerWindow::addMarker(Marker marker, int index) {
     QHBoxLayout *markerLayout = new QHBoxLayout();
     // Add number
@@ -85,7 +113,11 @@ QHBoxLayout* ViewMarkerWindow::addMarker(Marker marker, int index) {
     connect(select, SIGNAL(clicked()), this, SLOT(selectMarker()));
     return markerLayout;
 }
-
+/**
+ * @brief ViewMarkerWindow::setCurrentStep
+ * Simply changes what markerContainer contains.
+ * @param newIndex the new current index of the data we're on.
+ */
 void ViewMarkerWindow::setCurrentStep(int newIndex)
 {
     populateContainer(newIndex);
