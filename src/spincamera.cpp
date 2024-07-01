379ca8671e853c98e9cd13bfbc168413ca53e864
nathan desmee <nathan.desmee.etu@univ-lille.fr>
2024-07-01 17:04:51 +0200

    fix crash

--- src/spincamera.cpp
+++ src/spincamera.cpp
@@ -404,6 +404,7 @@ void SpinCamera::stopAutoCapture(){
 }
 
 unsigned char* SpinCamera::retrieveImage(unsigned int* bufferSize, unsigned int* imageWidth, unsigned int* imageHeight, bool colored) {
+
     if (capturing) return nullptr;
     capturing = true;
     //printf("Images begin to be retrieved\n");
@@ -412,8 +413,20 @@ unsigned char* SpinCamera::retrieveImage(unsigned int* bufferSize, unsigned int*
     if (!isAlreadyInit) {
         cam->Init();
     }
-    Spinnaker::TriggerModeEnums oldTriggerValue =  cam->TriggerMode.GetValue();
-    cam->TriggerMode.SetValue(TriggerModeEnums::TriggerMode_Off);
+
+    Spinnaker::TriggerModeEnums oldTriggerValue;
+
+    try {
+        oldTriggerValue = cam->TriggerMode.GetValue();
+        cam->TriggerMode.SetValue(TriggerModeEnums::TriggerMode_Off);
+    } catch (Spinnaker::Exception e) {
+        qInfo() << e.what();
+        if (!isAlreadyInit) {
+            cam = nullptr;
+        }
+        capturing = false;
+        return nullptr;
+    }
 
     //printf("Retrieving images...\n");
     //cam->AcquisitionStart.Execute(); <- Old version
