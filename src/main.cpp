f848c4df525a594d34ca4abc4299b6a6dcc02cbe
Armand Sady <armand.sady.etu@univ-lille.fr>
2024-07-03 14:52:23 +0200

    ensures light mode on windows no matter the actual color mode

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <QtWidgets/qapplication.h>
 
 int main(int argc, char** argv) {
+    #if defined(WIN32) || defined(_WIN32)
+        qputenv("QT_QPA_PLATFORM", "windows:darkmode=0");
+    #endif
     QApplication app(argc, argv);
     //app.setStyle("gtk+");
     //QFont serifFont("Times", 4);
     //app.setFont(serifFont);
     MainWindow window;
-
     // qInfo() << QApplication::primaryScreen()->size();
 
     window.show();
