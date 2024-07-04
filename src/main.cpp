41181f643a2354c95c8a080ceb47c0e89b3d2429
Armand Sady <armand.sady.etu@univ-lille.fr>
2024-07-04 11:46:39 +0200

    qpalette & style to ensure correct lightmode

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,28 @@ int main(int argc, char** argv) {
         qputenv("QT_QPA_PLATFORM", "windows:darkmode=0");
     #endif
     QApplication app(argc, argv);
-    //app.setStyle("gtk+");
+
+    app.setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette lightPalette;
+    lightPalette.setColor(QPalette::Window, QColor(255, 255, 255));
+    lightPalette.setColor(QPalette::WindowText, QColor(0, 0, 0));
+    lightPalette.setColor(QPalette::Base, QColor(255, 255, 255));
+    lightPalette.setColor(QPalette::AlternateBase, QColor(242, 242, 242));
+    lightPalette.setColor(QPalette::ToolTipBase, QColor(255, 255, 220));
+    lightPalette.setColor(QPalette::ToolTipText, QColor(0, 0, 0));
+    lightPalette.setColor(QPalette::Text, QColor(0, 0, 0));
+    lightPalette.setColor(QPalette::Button, QColor(255, 255, 255));
+    lightPalette.setColor(QPalette::ButtonText, QColor(0, 0, 0));
+    lightPalette.setColor(QPalette::BrightText, QColor(255, 0, 0));
+    lightPalette.setColor(QPalette::Link, QColor(0, 0, 255));
+    lightPalette.setColor(QPalette::Highlight, QColor(0, 120, 215));
+    lightPalette.setColor(QPalette::HighlightedText, QColor(255, 255, 255));
+
+    app.setPalette(lightPalette);
     //QFont serifFont("Times", 4);
     //app.setFont(serifFont);
     MainWindow window;
-    // qInfo() << QApplication::primaryScreen()->size();
-
     window.show();
     return app.exec();
 }
